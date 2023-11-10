f5911e7e84f091b5919c646e04534d49c538f4b5
ThomasvanderVeen <125651413+ThomasvanderVeen@users.noreply.github.com>
2023-11-10 10:08:46 +0100

    Update Config.py

--- ProductPredictor/Config.py
+++ ProductPredictor/Config.py
@@ -15,17 +15,13 @@ class Config:
     filename = config.get("General", "Last_step_name")
 
     layers = json.loads(config.get("Network Stress", "Architecture"))
-    N_steps = config.getint("Network Stress", "N_steps")
-    initial_learning_rate = config.getfloat("Network Stress", "Initial_learning_rate")    
-    decay_steps = config.getint("Network Stress", "Decay_steps")
-    decay_rate = config.getfloat("Network Stress", "Decay_rate")
+    N_steps = config.getint("Network Stress", "N_epochs")
+    initial_learning_rate = config.getfloat("Network Stress", "learning_rate")    
     validation_ratio = config.getfloat("Network Stress", "Validation_ratio")
 
     layers_coords = json.loads(config.get("Network Coordinates", "Architecture"))
-    N_steps_coords = config.getint("Network Coordinates", "N_steps")
-    initial_learning_rate_coords = config.getfloat("Network Coordinates", "Initial_learning_rate")    
-    decay_steps_coords = config.getint("Network Coordinates", "Decay_steps")
-    decay_rate_coords = config.getfloat("Network Coordinates", "Decay_rate")
+    N_steps_coords = config.getint("Network Coordinates", "N_epochs")
+    initial_learning_rate_coords = config.getfloat("Network Coordinates", "learning_rate")    
     validation_ratio_coords = config.getfloat("Network Coordinates", "Validation_ratio")
 
     N_collocation_points = config.getint("PCNN", "N_collocation_points")
